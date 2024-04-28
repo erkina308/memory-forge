64516462b2869b634fab3bc140912642bb7e7e7c
erkina308 <erkinayna@hotmail.co.uk>
2024-04-28 15:08:09 +0100

    connection.js set up for production

--- connection.js
+++ connection.js
@@ -1,12 +1,32 @@
-require("dotenv").config();
 const { Pool } = require("pg");
+// const pathToProd = `${__dirname}/./.env.production`;
+// require("dotenv").config({
+//   path: pathToProd,
+// });
+require("dotenv").config();
+
+// const db = new Pool({
+//   user: process.env.DB_USER,
+//   host: "localhost",
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: 5432,
+// });
+
+// const config = {};
+
+// config.connectionString = process.env.DATABASE_URL;
+// config.max = 2;
 
 const db = new Pool({
   user: process.env.DB_USER,
-  host: "localhost",
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = db;
